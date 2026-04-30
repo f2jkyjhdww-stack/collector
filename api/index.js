ec82652da7d21679b87c77cbb3b8fd1f72baca3a
f2jkyjhdww-stack <f2jkyjhdww@privaterelay.appleid.com>
2026-04-30 12:50:06 +0800

    Add API handler for data collection
    Implement an API handler for POST requests that logs data and responds with a success message.

--- api/index.js
+++ api/index.js
@@ -1,2 +1,14 @@
-# collector
-auto collect token
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    console.log("收到数据：", data);
+
+    return res.status(200).json({
+      code: 0,
+      msg: "success"
+    });
+  }
+
+  return res.status(200).send("collector running");
+}
